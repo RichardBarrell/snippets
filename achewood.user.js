465ed123351b3bd1af4cddf946d9196f343d2394
Richard Barrell <rchrd@brrll.co.uk>
2012-03-30 17:26:21 +0100

    UserScript for making reading Achewood a tiny bit more convenient. If you're not reading Achewood already, start.

---
+++ achewood.user.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name           Achewood Lazy
+// @namespace      https://github.com/RichardBarrell/snippets
+// @description    Keyboard nav for Achewood
+// @include        http://achewood.com/*
+// @version        0.0.1
+// ==/UserScript==
+
+var prevLink = document.querySelector("a.dateNav[title='Previous comic']");
+var nextLink = document.querySelector("a.dateNav[title='Next comic']");
+var comic_body = document.getElementById("comic_body");
+
+if (prevLink && nextLink) {
+	document.addEventListener("keydown", function(event) {
+		if (event.keyCode === 37) {
+			window.location = prevLink.getAttribute("href");
+		}
+		if (event.keyCode === 39) {
+			window.location = nextLink.getAttribute("href");
+		}
+	});
+}
+if (comic_body) {
+	var comic = comic_body.querySelector("img.comic");
+	comic_body.appendChild(document.createTextNode(comic.getAttribute("title")));
+}
+
