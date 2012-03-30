838a8f7db9f2a4ad592dd609a7740d0da4b5fa6d
Richard Barrell <rchrd@brrll.co.uk>
2012-03-30 17:38:07 +0100

    I wrote a thing in Haskell to make connecting to IRC servers with "netcat" more convenient.

---
+++ IRCat.hs
@@ -0,0 +1,47 @@
+module Main where
+
+-- This thing works very much like "netcat", but it understands just
+-- enough of RFC 1459 to be able to respond to PING messages on your
+-- behalf, because that's the most tedious part of using IRC by hand.
+
+import Data.List
+import System.IO
+import Network
+import Char
+import Control.Concurrent
+import Control.Concurrent.Chan
+import System.Environment
+import System.Exit
+
+main = withSocketsDo $ do
+   args <- getArgs
+   handle <- connectTo (args !! 0) (PortNumber $ fromInteger $ read $ args !! 1)
+   logchan <- newChan
+   loghandle <- openFile (args !! 2) AppendMode
+   hSetBuffering handle LineBuffering
+   hSetBuffering loghandle LineBuffering
+   forkIO $ bouncy handle logchan
+   forkIO $ dumpy loghandle logchan
+   runaround handle logchan
+
+dumpy loghandle logchan = do
+   line <- readChan logchan
+   hPutStrLn loghandle line
+   dumpy loghandle logchan
+
+bouncy handle logchan = do
+   line <- catch (hGetLine handle) (\_ -> exitWith ExitSuccess)
+   if "ping " `isPrefixOf` (map toLower line)
+      then do
+         hPutStrLn handle $ "PONG " ++ (drop 5 line)
+         bouncy handle logchan
+      else do
+         hPutStrLn stdout line
+         writeChan logchan line
+         bouncy handle logchan
+
+runaround handle logchan = do
+   line <- catch (hGetLine stdin) (\_ -> exitWith ExitSuccess)
+   hPutStrLn handle line
+   writeChan logchan line
+   runaround handle logchan
