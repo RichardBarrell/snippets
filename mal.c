57343c07936de9363e8e5e8eeee898bd0ce18839
Richard Barrell <rchrd@brrll.co.uk>
2012-03-30 17:33:00 +0100

    How much can I malloc() on this damn computer anyway?

---
+++ mal.c
@@ -0,0 +1,29 @@
+/* How much can I actually malloc? One argument, number of megabytes.
+ * gcc mal.c -o mal
+ * ./mal 512
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char **argv) {
+	int howmuch = 1 << 20;
+	if (argc >= 2) {
+		howmuch = atoi(argv[1]);
+		if (howmuch<1) {
+			fprintf(stderr, "At least one byte please.\n");
+			return 1;
+		}
+	}
+	unsigned char *tibet = malloc(howmuch);
+	if (tibet == NULL) {
+		fprintf(stderr, "Oh no that didn't malloc.\n");
+	}
+	int i;
+	for (i=0;i<howmuch;++i) {
+		tibet[i] = howmuch & 0xFF;
+	}
+	free(tibet);
+	printf("That worked.\n");
+	return 0;
+}
