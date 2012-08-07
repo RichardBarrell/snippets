6990086bd8188a931d39d223f9c5c7bb56bc0452
Richard Barrell <rchrd@brrll.co.uk>
2012-08-07 23:13:01 +0100

    Tiny program that emits one nul byte.

---
+++ nulbyte.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/* For some reason, ages ago, I found myself wanting a tiny program that would
+   print a single nul byte on stdout in the middle of a really ugly shell
+   script.
+
+   gcc -o nulbyte -Os nulbyte.c
+   strip nulbyte
+
+   # to test :P
+   ./nulbyte | od -t x1
+*/
+
+int main(int argc, const char** argv) {
+	putchar(0);
+	return 0;
+}
