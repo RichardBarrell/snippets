1ed27b8f9742a5ee432f4ad9dcf857dfd98a6620
Richard Barrell <rchrd@brrll.co.uk>
2012-08-23 13:52:13 +0100

    Tiny C program that pretty-prints argc and argv (for conservative values of "pretty", but eh whatever.)

---
+++ argcargv.c
@@ -0,0 +1,16 @@
+/* Pretty-prints argc and argv.
+ * gcc -o argcargv argcargv.c
+ * I wrote this because I can NEVER remember how they go. :)
+ * It's almost useful for debugging incorrect word splitting in shell scripts.
+ */
+
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+	int i;
+	printf("argc=%d\n", argc);
+	for (i=0; i<argc; i++) {
+		printf("argv[%d] = %s\n", i, argv[i]);
+	}
+	return 0;
+}
