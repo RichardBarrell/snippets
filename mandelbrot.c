a1907bded15d21d6ecab189e77b75febb700a604
Richard Barrell <rchrd@brrll.co.uk>
2013-03-02 04:31:19 +0000

    Playing with OpenMP: a little mandelbrot program.

---
+++ mandelbrot.c
@@ -0,0 +1,50 @@
+/* gcc mandelbrot.c -o mandelbrot -lm -Ofast -fopenmp */
+/* Draws a Mandelbrot into out.ppm. Mostly I'm just playing with OpenMP. */
+#include <complex.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+
+int main(int argc, char **argv) {
+    size_t width = 1920, height = 1080;
+
+    unsigned char *block = malloc(width*height*3);
+    if (block == NULL) { perror("Couldn't malloc block.\n"); abort(); }
+
+    FILE *out = fopen("out.ppm", "w");
+    if (out == NULL) { perror("Couldn't open out.ppm.\n"); abort(); }
+
+    float dx = 2.5f / width, dy = 2.0f / height;
+    size_t yi;
+
+#pragma omp parallel for private(yi) shared(block)
+    for (yi=0; yi<height; yi++) {
+        float y = -1 + (dy * (float)yi);
+        char *p = block + (3 * width * yi);
+        size_t xi;
+        for (xi=0; xi<width; xi++) {
+            char *po = p + (3 * xi);
+            float x = -2 + (dx * (float)xi);
+
+            complex float z = 0;
+            complex float c = x + y * I;
+            unsigned char n = 0;
+
+            while (cabs(z) < 2.0f) {
+                z = z*z + c;
+                if (++n == 255) { break; }
+            }
+
+            po[0] = 255 - n;
+            po[1] = 255 - n;
+            po[2] = 255 - n;
+        }
+    }
+#pragma omp end parallel
+
+
+    fprintf(out, "P6 %zu %zu 255\n", width, height);
+    fwrite(block, width*height, 3, out);
+    puts("done");
+    return 0;
+}
