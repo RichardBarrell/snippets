00aadd86d687d9f39195f255ba1471eb2a6368fd
Richard Barrell <rchrd@brrll.co.uk>
2013-11-07 16:56:36 +0000

    WideLines needs to (barely) cope with the fact that some things aren't UTF-8. char8 is not a real text encoding, but it'll do for dealing with bytes.

--- WideLines.hs
+++ WideLines.hs
@@ -4,11 +4,14 @@ module Main where
 -- ./foo | ./WideLines 80
 
 import System.Environment (getArgs)
+import System.IO (stdin, stdout, hSetEncoding, char8)
 
 main = do args <- getArgs
           let width = parseWidth args
+          hSetEncoding stdin char8
+          hSetEncoding stdout char8
           interact (unlines . map (take width) . lines)
 
 parseWidth [] = 120
 parseWidth (a:_) = case reads a of [(n, _)] -> n
-                                   _         -> 120
+                                   _        -> 120
