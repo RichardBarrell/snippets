b890e5b5e49732919a2266c301e1ab351cea28cc
Richard Barrell <rchrd@brrll.co.uk>
2013-11-07 21:39:24 +0000

    Shut the damn keyboard backlight off on this MacBook.

---
+++ keyboard_backlight_off.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+	FILE *f = fopen("/sys/class/leds/smc::kbd_backlight/brightness", "wb");
+	if (f == NULL) { printf("Can't open brightness.\n"); return 1; }
+	if (fwrite("0\n", 1, 2, f) != 2) { printf("Can't write brightness.\n"); return 1; }
+	fclose(f);
+	return 0;
+}
