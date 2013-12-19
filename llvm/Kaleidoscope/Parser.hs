a87b4d46049d7545fef025de99d9a90492798606
Richard Barrell <rchrd@brrll.co.uk>
2013-12-19 16:10:54 +0000

    Going to start following LLVM kaleidoscope tutorial along in Haskell. Haven't actually touched any of the LLVM stuff, only started on the lexer from the first chapter. :)

---
+++ llvm/Kaleidoscope/Parser.hs
@@ -0,0 +1,48 @@
+module Parser where
+
+import Text.Parsec
+import Control.Applicative ((<$>), (<*>))
+
+data Kt
+    = KtDef
+    | KtExtern
+    | KtId String
+    | KtNum Double
+    deriving (Show, Eq)
+
+ktokens :: Parsec String () [Kt]
+ktokens = tokens where
+    tokens = spaces >> (end <|> more)
+    more = (:) <$> (numToken <|> wordToken) <*> tokens
+    end = eof >> return []
+
+wordToken = do word <- many1 (noneOf " \t\r\n\f\v")
+               case word of "def"    -> return KtDef
+                            "extern" -> return KtExtern
+                            _ -> return (KtId word)
+
+numToken = readNumToken <$> many1 digit
+                        <*> numAfterPt
+                        <*> numExponent
+           where
+           readNumToken a b c = KtNum $ read (a ++ b ++ c)
+           numAfterPt = do char '.'
+                           digits <- many1 digit
+                           return $ '.' : digits
+                        <|> return ""
+           numExponent = do oneOf "eE"
+                            plusMinus <- fmap (:"") (oneOf "+-") <|> return ""
+                            digits <- many1 digit
+                            return $ 'e' : plusMinus ++ digits
+                         <|> return ""
+
+data Kast
+    = KaExp Kexp
+
+data Kexp
+    = KeNum Double
+    | KeVar String
+    | KeBin Kop Kexp Kexp
+    | KeCall String 
+    | KaVar String
+    | KaBinExp Kop Ka
