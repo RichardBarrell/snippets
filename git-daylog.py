5b9a6acc436fc3ea4bb7fce582dda7941723e7aa
Richard Barrell <rchrd@brrll.co.uk>
2015-12-09 13:19:00 +0000

    fix accidental pep8 in git-daylog.py

--- git-daylog.py
+++ git-daylog.py
@@ -4,8 +4,8 @@ import re
 import os
 
 if len(sys.argv) < 2 or not re.match(r"\d{4}-\d\d-\d\d", sys.argv[1]):
-	print "Usage: git daylog 2013-01-01 ..."
-        sys.exit(1)
+    print "Usage: git daylog 2013-01-01 ..."
+    sys.exit(1)
 
 day = sys.argv[1]
 after = "--after=%s 00:00" % day
